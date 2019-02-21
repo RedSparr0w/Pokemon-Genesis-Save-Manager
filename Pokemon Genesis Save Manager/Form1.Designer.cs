a700c3e3ac47347dbaac87534f7153f9211c37bb
Danial Nickford <admin@redsparr0w.com>
2019-02-21 17:55:19 +1300

    Set maximum and minimum sizes

--- Pokemon Genesis Save Manager/Form1.Designer.cs
+++ Pokemon Genesis Save Manager/Form1.Designer.cs
@@ -183,6 +183,8 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(843, 435);
+            this.MinimumSize = new System.Drawing.Size(843, 435);
             this.Name = "Form1";
             this.Text = "Pokemon Genesis Save Manager";
             this.contextMenuSaves.ResumeLayout(false);
